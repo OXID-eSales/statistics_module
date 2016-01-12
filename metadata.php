7fe8e7f0a45282c8ea69cb8460e7934946f61d12
kermie <info@marco-steinhaeuser.de>
2016-01-12 22:01:33 +0100

    metadata: title and description DE and EN

--- metadata.php
+++ metadata.php
@@ -33,12 +33,12 @@ $sMetadataVersion = '1.1';
 $aModule = array(
     'id'          => 'oestatistics',
     'title'       => array(
-        'de' => '[TR - OE Statistics]',
+        'de' => 'OE Statistiken',
         'en' => 'OE Statistics',
     ),
     'description' => array(
-        'de' => '[TR - OE Statistics Module]',
-        'en' => 'OE Statistics Module',
+        'de' => 'Ein Modul für das Loggen und die Ausgabe von Statistiken in OXID eShop Community und Professional Edition',
+        'en' => 'A module for logging and displaying statistics for OXID eShop Community and Professional Edition',
     ),
     'thumbnail'   => 'out/pictures/picture.png',
     'version'     => '1.0.0',
